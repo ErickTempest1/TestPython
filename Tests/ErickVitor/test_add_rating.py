124b40c3e2c38d4f0573828599c2c14ea1f4246c
Erick <erickvitor2705@gmail.com>
2025-11-22 15:24:44 -0400

    Atualize os orquivos alterados

--- Tests/ErickVitor/test_add_rating.py
+++ Tests/ErickVitor/test_add_rating.py
@@ -8,6 +8,11 @@ headers = {
     "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MWUzYWZlYzI3MWYxMjE3NzdiNjg1ZWUwNjMwYmRkNiIsIm5iZiI6MTc2MzYwMDMwNy4yNTgsInN1YiI6IjY5MWU2N2IzZGY0NTI0ZjgwZmMyY2JlYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oSN4WXIYsxv7t345_2_GXaSbCHInOa9Ub8vYiWU8nRA"
 }
 
+def test_can_get_tv_show_details():
+    url = "https://api.themoviedb.org/3/tv/1399?language=en-US"
+    response = requests.get(url, headers=headers)
+    assert response.status_code == 200
+
 def test_can_add_tv_show_rating():
     url = "https://api.themoviedb.org/3/tv/1399/rating"
     payload = {
@@ -15,11 +20,6 @@ def test_can_add_tv_show_rating():
     }
     response = requests.post(url, headers=headers, json=payload)
     assert response.status_code == 201
-
-def test_can_get_tv_show_details():
-    url = "https://api.themoviedb.org/3/tv/1399?language=en-US"
-    response = requests.get(url, headers=headers)
-    assert response.status_code == 200
 
 def test_can_delete_tv_show_rating():
     url = "https://api.themoviedb.org/3/tv/1399/rating"
